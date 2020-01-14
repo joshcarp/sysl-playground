b7562298ba5d93f6bae4d8dcf3cc725026a44377
joshcarp <joshuacarpeggiani@gmail.com>
2020-01-14 15:40:18 +1100

    Modify Playground

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"encoding/base64"
 	"errors"
 	"fmt"
-	"net/http"
-	"net/url"
 	"regexp"
-	"syscall/js"
 
+	"github.com/Joshcarp/sysl-playground/pkg/urls"
 	"github.com/Joshcarp/sysl_testing/pkg/command"
 	"github.com/gopherjs/vecty"
 	"github.com/gopherjs/vecty/elem"
@@ -17,63 +14,29 @@ import (
 	"github.com/spf13/afero"
 )
 
-var mychan = make(chan string, 10000)
-var mGlobal *Markdown
-var info *http.Response
-
-func encodeUrl(input, cmd string) string {
-	input = encode(input)
-	cmd = encode(cmd)
-	url := js.Global().Get("location").Get("hostname").String()
-	port := js.Global().Get("location").Get("port").String()
-	pathname := js.Global().Get("location").Get("pathname").String()
-
-	if port != "" {
-		port = ":" + port
-	}
-	// pathname  = strings.Replace(pathname, `/`,"", 1)
-	// if pathname != ""{
-	// 	pathname = `/` + pathname
-	// }
-
-	fmt.Println(pathname)
-	return fmt.Sprintf("http://%s%s/%s?input=%s&cmd=%s", url, port, pathname, input, cmd)
-
+// PageView is our main page component.
+type PageView struct {
+	vecty.Core
+	Input     string
+	Command   string
+	Link      string
+	InputLink string
 }
 
-func loadQueryParams() (url.Values, bool) {
-	href := js.Global().Get("location").Get("href")
-	str := fmt.Sprintf("%s", href)
-	u, err := url.Parse(str)
-	check(err)
-	if len(u.Query()) == 0 {
-		return u.Query(), false
-	}
-	return u.Query(), true
+func main() {
+	input, cmd := setup()
+
+	vecty.SetTitle("SYSL Playground")
+
+	vecty.RenderBody(&PageView{
+		Input:   input,
+		Command: cmd,
+	})
 }
 
-func encode(str string) string {
-	return base64.StdEncoding.EncodeToString([]byte(str))
-}
-
-func decode(str string) string {
-	this, _ := base64.StdEncoding.DecodeString(str)
-	return string(this)
-}
-func decodeQueryParams(in url.Values) (string, string) {
-	foo := decode(in.Get("input"))
-	bar := decode(in.Get("cmd"))
-	return foo, bar
-}
-func decodeURLString(in string) (string, string) {
-	u, err := url.Parse(in)
-	check(err)
-	return decodeQueryParams(u.Query())
-}
 func setup() (string, string) {
-	href, _ := loadQueryParams()
-	input, cmd := decodeQueryParams(href)
-	fmt.Println("command", cmd)
+	playgroundUrl, _ := urls.LoadQueryParams()
+	input, cmd := urls.DecodeQueryParams(playgroundUrl)
 
 	if input == "" {
 		input = `MobileApp:
@@ -91,41 +54,31 @@ Server:
 	if cmd == "" {
 		cmd = "sysl sd -o \"project.svg\" -s \"MobileApp <- Login\" tmp.sysl"
 	}
-
-	fmt.Println(cmd, input)
-	fmt.Println("2")
 	return input, cmd
 }
-func main() {
-	c := make(chan bool)
 
-	input, cmd := setup()
 
-	vecty.SetTitle("sysl playground")
-
-	vecty.RenderBody(&PageView{
-		Input:   input,
-		Command: cmd,
-	})
-	fmt.Println("5")
-
-	// go keepAlive()
-	<-c
-	select {}
-}
-
-// PageView is our main page component.
-type PageView struct {
-	vecty.Core
-	Input     string
-	Command   string
-	Link      string
-	InputLink string
-}
 
 // Render implements the vecty.Component interface.
 func (p *PageView) Render() vecty.ComponentOrHTML {
 	return elem.Body(
+		vecty.Markup(
+			vecty.Class("body"),
+		),
+		elem.Header(
+			vecty.Text("Sysl Playground"),
+			vecty.Markup(
+				vecty.Style("font-family", "monospace"),
+				vecty.Style("font-size", "25px"),
+			),
+		),
+		elem.Article(
+			vecty.Text("Welcome to the Sysl Playground"),
+			vecty.Markup(
+			vecty.Style("font-family", "monospace"),
+			),
+	),
+		
 		// Display a textarea on the right-hand side of the page.
 		elem.Table(
 			elem.TableRow(
@@ -174,29 +127,20 @@ func (p *PageView) Render() vecty.ComponentOrHTML {
 				vecty.Markup(
 					vecty.UnsafeHTML("Share"),
 					event.Click(func(e *vecty.Event) {
-						p.Link = encodeUrl(p.Input, p.Command)
+						p.Link = urls.EncodeUrl(p.Input, p.Command)
 						vecty.Rerender(p)
 					}),
 				),
 			),
-			// elem.Button(
-			// 	vecty.Markup(
-			// 		vecty.UnsafeHTML("Load"),
-			// 		event.Click(func(e *vecty.Event) {
-			// 			p.Input, p.Command = decodeURLString(p.InputLink)
-			// 			vecty.Rerender(p)
-			// 		}),
-			// 	),
-			// ),
 		),
 		elem.TableRow(
 			elem.TableData(
 				elem.TextArea(
 					vecty.Markup(
 						vecty.Style("font-family", "monospace"),
-						vecty.Property("rows", 2),
+						vecty.Property("rows", 7),
 						vecty.Property("cols", 70),
-						vecty.Style("wrap", "hard"),
+						vecty.Property("wrap", "hard"),
 						event.Input(func(e *vecty.Event) {
 							p.InputLink = e.Target.Get("value").String()
 						}),
@@ -219,6 +163,7 @@ type Markdown struct {
 // Render implements the vecty.Component interface.
 func (m *Markdown) Render() (res vecty.ComponentOrHTML) {
 	defer func() {
+		// If panic, then print the error
 		if r := recover(); r != nil {
 			res = elem.Div(
 				vecty.Markup(
@@ -227,35 +172,41 @@ func (m *Markdown) Render() (res vecty.ComponentOrHTML) {
 			)
 		}
 	}()
-	fs := afero.NewMemMapFs()
-	re := regexp.MustCompile(`\w*\.sysl`)
 
+	// Declare a Memory filesystem and Logger
+	fs := afero.NewMemMapFs()
+	logger := logrus.New()
+
+	// Replace any input files with /tmp.sysl
+	re := regexp.MustCompile(`\w*\.sysl`)
 	m.Command = re.ReplaceAllString(m.Command, "/tmp.sysl")
 
-	re = regexp.MustCompile(`(?m)(?:-o)\s"?([\S]+)`)
-	// fmt.Println("this this ", m.Command)
-	m.Command = re.ReplaceAllString(m.Command, "-o project.svg")
 	f, err := fs.Create("/tmp.sysl")
 	check(err)
 
+	// Write String input to file
 	_, e := f.Write([]byte(m.Input))
 	check(e)
 
-	var logger = logrus.New()
+	// Replace any output files with project.svg
+	re = regexp.MustCompile(`(?m)(?:-o)\s"?([\S]+)`)
+	m.Command = re.ReplaceAllString(m.Command, "-o project.svg")
+	
 	args, err := parseCommandLine(m.Command)
 	check(err)
-	fmt.Println(args, len(args))
+
+	// Execute sysl
 	command.Main2(args, fs, logger, command.Main3)
 
-	this, err := afero.ReadFile(fs, "project.svg")
+	output, err := afero.ReadFile(fs, "project.svg")
 	check(err)
 
-	foo := fmt.Sprintf("<img src=\"%s\">", string(this))
-	fmt.Println(foo)
+	image := fmt.Sprintf("<img src=\"%s\">", string(output))
+
 	return elem.Div(
 		vecty.Markup(
 			vecty.UnsafeHTML(
-				foo),
+				image),
 		),
 	)
 }
@@ -266,8 +217,7 @@ func check(err error) {
 	}
 }
 
-var signal = make(chan int)
-
+// parseCommandLine is from user laurent on stackoverflow to split commands into string slices
 func parseCommandLine(command string) ([]string, error) {
 	var args []string
 	state := "start"
