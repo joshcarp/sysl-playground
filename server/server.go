8645c19f5627c38e185f085afe7c7adf48c22b22
joshcarp <joshuacarpeggiani@gmail.com>
2020-01-14 15:53:57 +1100

    Update server

--- server/server.go
+++ server/server.go
@@ -1,26 +1,14 @@
-// package main
-
-// import (
-// 	"flag"
-// 	"log"
-// 	"net/http"
-// )
-
 package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
-	"os/exec"
-
-	"github.com/jaschaephraim/lrserver"
-	"gopkg.in/fsnotify.v1"
 )
 
-func server() {
-	port := flag.String("p", "2021", "port to serve on")
+
+func main() {
+	port := flag.String("p", "3030", "port to serve on")
 	directory := flag.String("d", ".", "the directory of static file to host")
 	flag.Parse()
 
@@ -28,51 +16,4 @@ func server() {
 
 	log.Printf("Serving %s on HTTP port: %s\n", *directory, *port)
 	log.Fatal(http.ListenAndServe(":"+*port, nil))
-}
-
-const html = `<!doctype html>
-<html>
-<head>
-<script src="http://localhost:35729/livereload.js"></script>
-</head>
-</html>`
-
-func main() {
-	// Create file watcher
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer watcher.Close()
-
-	// Add dir to watcher
-	err = watcher.Add(".")
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	// Create and start LiveReload server
-	lr := lrserver.New(lrserver.DefaultName, lrserver.DefaultPort)
-	go lr.ListenAndServe()
-
-	// Start goroutine that requests reload upon watcher event
-	go func() {
-		for {
-			select {
-			case event := <-watcher.Events:
-				fmt.Println(event)
-				cmd := exec.Command("make")
-				cmd.Run()
-				lr.Reload("index.html")
-			case err := <-watcher.Errors:
-				log.Println(err)
-			}
-		}
-	}()
-	// server()
-	http.HandleFunc("/", func(rw http.ResponseWriter, req *http.Request) {
-		rw.Write([]byte(html))
-	})
-	http.ListenAndServe(":3000", nil)
-
 }
